c64291e5d3ff8a9379b247303b9c784f3c5f8411
Manuel Restrepo <manolo.1217@hotmail.com>
2022-11-28 20:39:45 -0500

    basic types

---
+++ src/ejercicios/01-tipos-basicos.ts
@@ -0,0 +1,12 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+
+let nombre: string = "Manuel";
+
+let numero: number | string= 25;
+let si: boolean = true;
+numero = 'palabra';
+
+console.log(nombre, numero, si);
